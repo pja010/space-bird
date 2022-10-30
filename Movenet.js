6dee441bf352f8263f9f0b74643b63697269e059
Glitch (glitch-hello-website) <none>
2022-10-30 19:32:44 +0000

    🚉💾 Checkpoint ./Movenet.js:49783677/36 ./index.html:49783677/93:50252695/39

--- Movenet.js
+++ Movenet.js
@@ -1,15 +1,15 @@
-let video;
-let skeleton;
+// let video;
+// let skeleton;
 
-function setup(){
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.hide();
+// function setup(){
+//   createCanvas(640, 480);
+//   video = createCapture(VIDEO);
+//   video.hide();
   
-}
-async function asyncCall(){
+// }
+// async function asyncCall(){
   
-  const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
-  const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
-  const poses = await detector.estimatePoses(image);
-}
+//   const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
+//   const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+//   const poses = await detector.estimatePoses(image);
+// }
